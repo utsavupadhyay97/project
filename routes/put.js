f18e961eacf36ccf13bf196bdfc86e07bececf66
utsavupadhyay97 <82389687+utsavupadhyay97@users.noreply.github.com>
2021-04-12 20:32:55 +0545

    Update put.js

--- routes/put.js
+++ routes/put.js
@@ -5,7 +5,7 @@ const AboutUs = require('../model/aboutUS');
 
 
 router.patch('/aboutus/:postid', async (req,res) => {
-   
+   ///sab ko lagi hai sak xas vanee bro about ta gare tesarinai aru ko pani
     //create new address
     try{
         const updatepost = await AboutUs.updateMany({_id:req.params.postid},
